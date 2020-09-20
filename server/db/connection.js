3bc86bdab39b3d6afa1b5262c26af9c0aed8bfb9
Kavya Tripathi <48882133+kavyatripathi@users.noreply.github.com>
2020-09-20 11:38:32 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/photo_gallery', {
+mongoose.connect('mongodb://127.0.0.1:27017/file_mnagement', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
